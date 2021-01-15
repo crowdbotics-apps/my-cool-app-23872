7d4284aed384ec40c99df08506b6f45cf0ffad7f
John <45923790+jbenderdev@users.noreply.github.com>
2021-01-15 18:51:39 -0500

    Update mainNavigator.js

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -6,6 +6,7 @@ import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
 import CbSplashScreen1193684Navigator from '../features/CbSplashScreen1193684/navigator';
+import OnboardingSlideScreen2193683Navigator from '../features/OnboardingSlideScreen2193683/navigator';
 /**
  * new navigators can be imported here
  */
@@ -14,6 +15,7 @@ const AppNavigator = {
 
     //@BlueprintNavigationInsertion
     CbSplashScreen1193684: { screen: CbSplashScreen1193684Navigator },
+    OnboardingSlideScreen2193683: { screen: OnboardingSlideScreen2193683Navigator },
     /** new navigators can be added here */
     SplashScreen: {
       screen: SplashScreen
