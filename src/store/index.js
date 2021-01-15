89b5279f85a26cc90f841e8c01ffe92424f0cdc3
John <45923790+jbenderdev@users.noreply.github.com>
2021-01-15 18:56:40 -0500

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,8 @@ import apiReducer from "./reducers";
 import customReducer from "./custom/reducers"
 import rootSaga from "./sagas";
 import customRootSaga from "./custom/sagas"
+import authRootSaga from './auth/sagas';
+import authReducer from './auth/reducers';
 
 import { combineReducers, createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
@@ -18,7 +20,8 @@ const middlewares = [sagaMiddleware /** more middlewares if any goes here */];
 const store = createStore(
   combineReducers({
       apiReducer: apiReducer,
-      customReducer: customReducer
+      customReducer: customReducer,
+      authReducer: authReducer
   }),
   composeEnhancers(applyMiddleware(...middlewares))
 );
