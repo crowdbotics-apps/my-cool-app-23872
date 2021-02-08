f6b82a3b51e093a3f9e2733fa300faa4773fa904
Crowdbotics <team@crowdbotics.com>
2021-02-08 20:28:45 +0000

    add package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'BlankScreen3202348', human_name: 'Blank Screen 3', access_route: 'BlankScreen3202348'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import BlankScreen3202348Navigator from '../features/BlankScreen3202348/navigator';
 import CbSplashScreen1193684Navigator from '../features/CbSplashScreen1193684/navigator';
 import OnboardingSlideScreen2193683Navigator from '../features/OnboardingSlideScreen2193683/navigator';
 import BasicLoginSignup3193682Navigator from '../features/BasicLoginSignup3193682/navigator';
@@ -15,6 +16,7 @@ import BasicLoginSignup3193682Navigator from '../features/BasicLoginSignup319368
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+BlankScreen3202348: { screen: BlankScreen3202348Navigator },
     CbSplashScreen1193684: { screen: CbSplashScreen1193684Navigator },
     OnboardingSlideScreen2193683: { screen: OnboardingSlideScreen2193683Navigator },
     BasicLoginSignup3193682: { screen: BasicLoginSignup3193682Navigator },
